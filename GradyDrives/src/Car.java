be9298c53391d38861a25d800e006934c2826306
ggmaddr <102542221+ggmaddr@users.noreply.github.com>
2022-11-29 03:15:30 -0800

    Update Car.java

--- GradyDrives/src/Car.java
+++ GradyDrives/src/Car.java
@@ -1,22 +1,38 @@
 import javax.swing.*;
-
-public class Car extends Vehicles {
+public class Car extends Vehicles{
     private ImageIcon carIcon;
-
-    Car(int positionX, int positionY, int speed, String type) {
-        super(positionX, positionY, speed, type);
+    Car(String type) {
+        super(type);
     }
-
     public void setCarIcon(ImageIcon carIcon) {
         this.carIcon = carIcon;
     }
-
     public ImageIcon getCarIcon() {
         return carIcon;
     }
-
     @Override
-    void move() {
+    void move(char command) {
+        switch (command)
+        {
+            case 'l':
+            {
+                setxPos(getxPos() - 100);
 
+                if (getxPos() < 100)
+                {
+                    setxPos(100);
+                }
+            }
+            case 'r':
+            {
+                setxPos(getxPos() + 100);
+
+                if (getxPos() > 500)
+                {
+                    setxPos(500);
+                }
+            }
+
+        }
     }
 }
