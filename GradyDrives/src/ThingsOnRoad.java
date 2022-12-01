e8dbbe5a74d358e81497724d9bae3a3b975cb365
ggmaddr <102542221+ggmaddr@users.noreply.github.com>
2022-12-01 06:03:03 -0800

    Update ThingsOnRoad.java

--- GradyDrives/src/ThingsOnRoad.java
+++ GradyDrives/src/ThingsOnRoad.java
@@ -2,4 +2,7 @@ public interface ThingsOnRoad {
    //things on road can be obstacles or gift for the player
     void function();
 
+    void randomPositionGenerator();
+    int getVerticleDistance();
+
 }
